b0e185bbc46023ddfc27f1da8324ddf80bf50cf5
misterlengzzz <62778032+misterlengzzz@users.noreply.github.com>
2021-02-11 17:52:18 +0700

    Add Category Crud

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
+use App\Post;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +25,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $categories = Category::orderBy('id', 'DESC')->limit('3')->get();
+        $posts = Post::orderBy('id', 'DESC')->where('post_type', 'post')->limit('3')->get();
+        $pages = Post::orderBy('id', 'DESC')->where('post_type', 'page')->limit('3')->get();
+        return view('admin.index', compact('categories', 'posts', 'pages'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
+Route::get('/', 'WebsiteController@index')->name('index');
+Route::get('category/{slug}', 'WebsiteController@category')->name('category');
+Route::get('post/{slug}', 'WebsiteController@post')->name('post');
+Route::get('page/{slug}', 'WebsiteController@page')->name('page');
+Route::get('contact', 'WebsiteController@showContactForm')->name('contact.show');
+Route::post('contact', 'WebsiteController@submitContactForm')->name('contact.submit');
+
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function () {
+    Route::resource('categories', 'CategoryController');
+    Route::resource('posts', 'PostController');
+    Route::resource('pages', 'PageController');
+    Route::resource('galleries', 'GalleryController');
+});
+
+
+
